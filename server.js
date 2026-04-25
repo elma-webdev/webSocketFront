54668b0f2c2edfd3326f59fa28d70a11f91ed484
elma-devweb <elmaeira@gmail.com>
2026-04-25 20:48:29 +0100

    add: file server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const WebSocket = require("ws");
+
+// Create a WebSocket server on port 8080
+const wss = new WebSocket.Server({ port: 8080 });
+
+console.log("WebSocket server is running on ws://localhost:8080");
+
+// Connection event handler
+wss.on("connection", (ws) => {
+
+  ws.id=Math.random().toString(36).substr(2, 9); // Gerar um ID único 
+
+  ws.send(
+    JSON.stringify({
+      type: "SYSTEM",
+      id: ws.id,
+      message: `Bem-vindo, User ${ws.id} 😜!`,
+    }),
+  );
+
+ ws.on("message", (data) => {
+   // O 'data' chega como Buffer, precisamos converter para String
+   const mensagem = data.toString();
+
+   wss.clients.forEach((client) => {
+     if (client.readyState === WebSocket.OPEN) {
+       client.send(JSON.stringify({ type:'USER', id: ws.id, message: mensagem, timestamp: new Date().toISOString()}));
+     }
+   });
+ });
+
+  // Close event handler
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+  
+});
