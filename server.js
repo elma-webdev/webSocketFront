6a7b8739e1a8291d1c7f3e24e718625b60cd712a
elma-devweb <elmaeira@gmail.com>
2026-04-25 20:58:26 +0100

    fix: change port to process.env.PORT

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const WebSocket = require("ws");
-
+const PORT = process.env.PORT || 8080;
 // Create a WebSocket server on port 8080
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: PORT });
 
-console.log("WebSocket server is running on ws://localhost:8080");
+console.log("WebSocket server is running on ws://localhost:" + PORT);
 
 // Connection event handler
 wss.on("connection", (ws) => {
